53f9dfe8f0c7e7d27cbb15ce3be68f5409de3054
Jansseen limsiaco <janz@Jansseens-MacBook-Air.local>
2022-03-24 17:15:49 +0800

    filtered when selecting userid

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,17 +3,16 @@ import React, { useState } from 'react';
 import { Row, Col, Form } from 'react-bootstrap';
 import _ from 'lodash';
 
-const FiltePage = ({ posts }) => {
-  const options = _.chain(posts).map('userId').uniq().value();
-
-  const [userId, setUserId] = useState(options[0]);
-  console.log('FiltePage', userId);
+const FiltePage = ({ filteredDataByUserId,options }) => {
+  const handleOnChange = (e) =>{
+    filteredDataByUserId(e.target.value)
+  }
 
   return (
     <Row className='row-cols-2'>
       <form>
         <Col>
-          <Form.Select size="sm" onChange={e => setUserId(e.target.value)} value={userId}>
+          <Form.Select size="sm" onChange={handleOnChange}>
             {options.map(o => (
               <option key={o} value={o}>{o}</option>
             ))}
--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import _ from 'lodash';
 import { Row, Col } from 'react-bootstrap';
 import Posts from '../components/Posts';
 import Filter from '../components/Filter';
@@ -6,20 +7,28 @@ import Filter from '../components/Filter';
 
 function FeedPage () {
   // const [isLoading, setIsLoading] = React.useState(true);
+  const [dataOriginal, setDataOriginal] = React.useState([]);
   const [data, setData] = React.useState([]);
 
   useEffect(() => {
     const url = 'https://jsonplaceholder.typicode.com/posts';
     fetch(url)
       .then((response) => response.json())
-      .then((json) => setData(json))
+      .then((json) => {
+        setData(json)
+        setDataOriginal(json)
+      })
       .catch((error) => console.log(error));
   }, []);
-
+  const filteredDataByUserId = (value) => {
+    const filteredUserId = dataOriginal.filter((item) => item.userId === parseInt(value))
+    setData(filteredUserId)
+  }
+  const options = _.chain(dataOriginal).map('userId').uniq().value();
   return (
     <Row className='row-cols-1'>
       <Col>
-        <Filter posts={data} />
+        <Filter filteredDataByUserId={filteredDataByUserId} options={options} />
         <Posts posts={data} />
       </Col>
     </Row>
